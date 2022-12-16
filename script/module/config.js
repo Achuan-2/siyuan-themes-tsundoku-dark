5a590156c6418030e4a7e23a8560c8c298943180
Achuan-2 <achuan-2@outlook.com>
2022-12-17 00:34:37 +0800

    :lipstick: 取消在新窗口打开并聚焦

--- script/module/config.js
+++ script/module/config.js
@@ -1361,7 +1361,7 @@ export var config = {
                                 },
                             },
                             {
-                                enable: true,
+                                enable: false,
                                 type: null,
                                 mode: 'button',
                                 icon: '#iconFocus',
