2f24322cb6ec1161a87ad5f1b805db5764b83d96
Achuan-2 <achuan-2@outlook.com>
2022-12-17 10:44:54 +0800

    :lipstick: 取消Dark+主题的归档页签功能

--- script/module/config.js
+++ script/module/config.js
@@ -2365,7 +2365,7 @@ export var config = {
                     // 标签页菜单项
                     {
                         // 归档所有打开的标签页
-                        enable: true,
+                        enable: false,
                         prefixSeparator: true,
                         suffixSeparator: false,
                         id: 'theme-menu-tabbar-archive',
